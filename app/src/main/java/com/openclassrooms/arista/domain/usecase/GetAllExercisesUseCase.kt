8daf1292558e5154679588b537bb13464df59fcc
MARC NAVARRO <navamarc@gmail.com>
2025-07-08 21:52:29 +0200

    Update GetAllExercisesUseCase filterd by Id

--- app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
+++ app/src/main/java/com/openclassrooms/arista/domain/usecase/GetAllExercisesUseCase.kt
@@ -1,11 +1,16 @@
 package com.openclassrooms.arista.domain.usecase
 
 import com.openclassrooms.arista.data.repository.ExerciseRepository
+import com.openclassrooms.arista.data.repository.UserRepository
 import com.openclassrooms.arista.domain.model.Exercise
 import javax.inject.Inject
 
-class GetAllExercisesUseCase @Inject constructor(private val exerciseRepository: ExerciseRepository) {
+class GetAllExercisesUseCase @Inject constructor(
+    private val exerciseRepository: ExerciseRepository,
+    private val userRepository: UserRepository
+) {
     suspend fun execute(): List<Exercise> {
-        return exerciseRepository.getAllExercises()
+        val userId = userRepository.getAllUsers().firstOrNull()?.id
+        return exerciseRepository.getAllExercises().filter { it.userId == userId }
     }
 }
